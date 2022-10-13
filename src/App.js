e4275d78bd61b20e653f3bcbb2e8bc17134a360b
Shubham9455 <shubham.jaiswal.ece21@itbhu.ac.in>
2022-10-13 14:50:16 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
+
+import Final from './tutorial/1-useState/setup/1-error-example'
+import Setup from './tutorial/1-useState/setup/2-useState-basics'
+// import Final from './tutorial/1-useState/final/1-error-example'
+
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      <Setup></Setup>
+      {/* <Final></Final> */}
+      {/* <h2>Advanced Tutorial</h2> */}
     </div>
   )
 }
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  var [text,setText] = useState(0);
+  // const handleClick = ({num}) => {
+    
+  // }
+  function handleClick(num){
+    if (num ===0) {
+      text += 1;
+    } else {
+      text -= 1;
+    }
+    console.log(num);
+    setText(text);
+  }
+  return <>
+    <h1>{text}</h1>
+    <button className='btn' onClick = {()=>handleClick(0)}>Increase Number</button>
+    <button className='btn' onClick = {()=>handleClick(1)}>Decrease Number</button>
+  </>;
 };
 
 export default UseStateBasics;
